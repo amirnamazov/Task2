e07e63f0dd51f60406212ddb8123b762d55e4311
amir <amir1990@gmail.com>
2023-11-04 15:13:59 +0400

    Initial commit

---
+++ app/src/main/java/com/example/task2/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.example.task2.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T : ViewBinding>(
+    private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> T) : Fragment() {
+
+    protected val binding: T get() = _binding!!
+
+    private var _binding: T? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/task2/drawer/FragmentA.kt
@@ -0,0 +1,8 @@
+package com.example.task2.drawer
+
+import com.example.task2.base.BaseFragment
+import com.example.task2.databinding.FragmentABinding
+
+
+class FragmentA : BaseFragment<FragmentABinding>(FragmentABinding :: inflate) {
+}
